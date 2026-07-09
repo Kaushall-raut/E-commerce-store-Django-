5d0f3bc8a0cf0ea039687165665498e76a8eeb48
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-09 08:04:12 +0530

    manageing env files and migrations

--- backend/store/models.py
+++ backend/store/models.py
@@ -33,7 +33,7 @@ class UserProfile(models.Model):
 class Order(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
-    total_amount=models.DecimalField(max_digits=True,decimal_places=2)
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
 
     def __str__(self):
         return f"Order{self.id} by {self.user.username}"
