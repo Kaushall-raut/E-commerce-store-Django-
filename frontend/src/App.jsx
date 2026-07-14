495ce56e62f643473609d6337ae2cceabdf33045
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-14 19:33:18 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
+import {Router,Route,Routes} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <ProductList />
-    </>
+    <Router>
+      <Routes>
+        <Route  path="/" element= {<ProductList/>}  />
+        <Route  path="/product/:id" element= {<ProductDetail/>}  />
+
+      </Routes>
+    </Router>
   );
 };
 export default App;
