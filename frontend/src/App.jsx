c5619264a0f4cecd7bb60a52af4d7dfe316b3d5f
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-16 09:12:25 +0530

    product detail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
-import {Router,Route,Routes} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 const App = () => {
   return (
--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -35,7 +35,36 @@ const ProductDetail = () => {
     return <h2>No products found</h2>;
   }
 
-  
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center py-10">
+      <div className="bg-white shadow-lg rounded-2xl p-8 max-w-3xl w-full">
+        <div className=" flex flex-col md:flex-row gap-8">
+          <img
+            src={`${product.image}`}
+            alt={product.name}
+            className="w-full h-auto object-cover rounded-lg md:w-1/2"
+          />
+          <div className="flex-1">
+            <h1 className="text-3xl font-bold text-gray-800 mb-2">
+              {product.name}
+            </h1>
+            <p className="text-2xl font-semibold text-green-600 mb-6">
+              {product.price}
+            </p>
+            <button className="bg-blue-600 text-white px-6 py-2 transition rounded-lg hover:bg-blue-700">
+              Add to cart
+            </button>
+            <div className="mt-4">
+              <a href="/" className="text-blue-600 hover:underline ">
+                
+                Back to home
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
