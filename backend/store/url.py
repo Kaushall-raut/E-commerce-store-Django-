79a2b30c3c2b766a78124407656d7e868784a50a
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-18 08:53:22 +0530

    functions for cart functionality

--- backend/store/url.py
+++ backend/store/url.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path("products",views.get_product),
     path('category',views.get_categories),
     path("products/<int:pk>",views.get_products),
+    path("cart/",views.get_cart),
+    path("products/add",views.add_to_cart),
+    path("products/remove",views.remove_item_from_cart),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Products , Category
+from .models import Products , Category ,Cart,CartItem
 from rest_framework.response import Response
-from .serializer import ProductSerializer,CategorySerializer
+from .serializer import ProductSerializer,CategorySerializer,CartSerializer,CartItemSerializer
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
@@ -26,3 +26,30 @@ def get_products(request,pk):
         return Response(serializer.data)
     except Products.DoesNotExist:
         return Response({'error':'Product not found'},status=404)
+    
+@api_view(['GET'])
+def get_cart(request):
+    cart,created=Cart.objects.get_or_create(user=None)
+    serializer=CartSerializer(cart)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_to_cart(request):
+    product_id=request.data.get('product_id')
+    product=Products.objects.get(id=product_id)
+    cart,created=Cart.objects.get_or_create(user=None)
+    item,created=CartItem.objects.get_or_create(cart=cart,product=product)
+
+    if not created:
+        item.quantity+=1
+        item.save()
+
+        return Response({'message':'product added to cart','cart':CartSerializer(cart).data})
+
+@api_view(['POST'])
+def remove_item_from_cart(request):
+    item_id=request.data.get('item_id')
+    CartItem.objects.filter(id=item_id).delete()
+
+    return Response({'message':'Item remove from cart'})
+    
