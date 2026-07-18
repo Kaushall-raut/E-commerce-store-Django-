3228931da5da471dd705decc5296f69c83fea1e5
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-18 08:36:36 +0530

    add cart serializers

--- backend/store/serializer.py
+++ backend/store/serializer.py
@@ -1,4 +1,4 @@
-from .models import Products,Category
+from .models import Products,Category,CartItem,Cart
 from rest_framework import serializers
 
 
@@ -11,4 +11,21 @@ class ProductSerializer(serializers.ModelSerializer):
     category=CategorySerializer(read_only=True)
     class Meta:
         model=Products
+        fields='__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_name=serializers.CharField(source='products.name',read_only=True)
+    product_price=serializers.DecimalField(source='products.price',max_digits=10,decimal_places=2,read_only=True)
+    product_image=serializers.ImageField(source='product.image',read_only=True)
+
+    class Meta:
+        model=CartItem
+        fields='__all__'
+    
+class CartSerializer(serializers.ModelSerializer):
+    items=CartItemSerializer(many=True,read_only=True)
+    total=serializers.ReadOnlyField()
+
+    class Meta:
+        model=Cart
         fields='__all__'
